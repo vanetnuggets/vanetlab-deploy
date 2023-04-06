10f94714da78c2e0b8a1ab5fb789f5ff992bf817
omartinka <ondrejmar23@gmail.com>
2023-04-06 15:06:56 +0200

    run in host network

--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# TODO the same but `.bat` for our beloved windows fans
-git clone https://github.com/vanetnuggets/vanetlab-be
-git clone https://github.com/vanetnuggets/vanetlab-ns3
-
-docker-compose up -d
-docker-compose run --rm app
